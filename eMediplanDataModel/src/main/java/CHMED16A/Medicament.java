48e0f9222eb0b41fd6ecb1858090575a4bf53d18
k-tipp <kevin.m.t@hotmail.com>
2017-03-13 22:46:29 +0100

    Added all base classes

--- eMediplanDataModel/src/main/java/CHMED16A/Medicament.java
+++ eMediplanDataModel/src/main/java/CHMED16A/Medicament.java
@@ -34,4 +34,107 @@ public class Medicament {
 		this.autoMed = autoMed;
 	}
 
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getIdType() {
+		return idType;
+	}
+
+	public void setIdType(int idType) {
+		this.idType = idType;
+	}
+
+	public Iterable<Posology> getPos() {
+		return pos;
+	}
+
+	public void setPos(Iterable<Posology> pos) {
+		this.pos = pos;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public String getTkgRsn() {
+		return tkgRsn;
+	}
+
+	public void setTkgRsn(String tkgRsn) {
+		this.tkgRsn = tkgRsn;
+	}
+
+	public String getAppInstr() {
+		return appInstr;
+	}
+
+	public void setAppInstr(String appInstr) {
+		this.appInstr = appInstr;
+	}
+
+	public int getAutoMed() {
+		return autoMed;
+	}
+
+	public void setAutoMed(int autoMed) {
+		this.autoMed = autoMed;
+	}
+
+	public String getPrscbBy() {
+		return prscbBy;
+	}
+
+	public void setPrscbBy(String prscbBy) {
+		this.prscbBy = prscbBy;
+	}
+
+	public String getRoa() {
+		return roa;
+	}
+
+	public void setRoa(String roa) {
+		this.roa = roa;
+	}
+
+	public int getRep() {
+		return rep;
+	}
+
+	public void setRep(int rep) {
+		this.rep = rep;
+	}
+
+	public int getSubs() {
+		return subs;
+	}
+
+	public void setSubs(int subs) {
+		this.subs = subs;
+	}
+
+	public float getNbPack() {
+		return nbPack;
+	}
+
+	public void setNbPack(float nbPack) {
+		this.nbPack = nbPack;
+	}
+
+	public Iterable<PrivateField> getpFields() {
+		return pFields;
+	}
+
+	public void setpFields(Iterable<PrivateField> pFields) {
+		this.pFields = pFields;
+	}
 }
--- eMediplanDataModel/src/main/java/CHMED16A/Recommendation.java
+++ eMediplanDataModel/src/main/java/CHMED16A/Recommendation.java
@@ -1,5 +1,36 @@
 package CHMED16A;
 
 public class Recommendation {
+    private String id;
 
+    private int patAgr;
+    private String rmk;
+
+    public Recommendation(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getPatAgr() {
+        return patAgr;
+    }
+
+    public void setPatAgr(int patAgr) {
+        this.patAgr = patAgr;
+    }
+
+    public String getRmk() {
+        return rmk;
+    }
+
+    public void setRmk(String rmk) {
+        this.rmk = rmk;
+    }
 }
